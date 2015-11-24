9bdeef26404926bd6e6a9590780e4282d1bffd9a
Akos Maurer-Somogyi <mrakosms@gmail.com>
2015-11-24 09:27:03 +0100

    last element will zero, negative problem

--- week-3/day-1/roman_arabic.py
+++ week-3/day-1/roman_arabic.py
@@ -8,6 +8,7 @@ rn = rn.replace('V', '5 ')
 rn = rn.replace('I', '1 ')
 rn = rn.split()
 rn = list(map(int, rn))
+rn.append(0) # last element
 num = len(rn) # max 15
 i = 0
 
@@ -15,11 +16,6 @@ for i in range(i-1, num):
     if int(rn[i-1]) < int(rn[i]):
         rn[i-1] = -(rn[i-1])
     i += 1
-else:
-    pass
-# last element
-if rn[-1] < 0:
-    rn[-1] = rn[-1] * -1
 else:
     pass
 
