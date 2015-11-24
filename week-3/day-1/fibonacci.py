624a18ca081e5dd98f35c0485614a4abea303707
Akos Maurer-Somogyi <mrakosms@gmail.com>
2015-11-24 12:52:12 +0100

    correct math

--- week-3/day-1/fibonacci.py
+++ week-3/day-1/fibonacci.py
@@ -3,7 +3,7 @@ i = 0
 
 last = int(input('How many fibonacci numbers would you like to see? Enter the number: '))
 
-for i in range(0, last-2):
+for i in range(0, last-1):
     num = len(fibonacci)
     fibonacci.append(fibonacci[num-1]+fibonacci[num-2])
 print(fibonacci)
