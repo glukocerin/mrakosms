30ab01b4463a9db9834b5d1427c3749ce920743c
Akos Maurer-Somogyi <mrakosms@gmail.com>
2015-11-27 00:12:55 +0100

    check lenght, print words that contains palindrome

---
+++ week-3/demo2.py
@@ -0,0 +1,39 @@
+# 2nd Part:
+#
+# Create a function that searches for all the palindromes in a string that are longer than 3 characters, and returns a list with the found palindromes. Example:
+#
+# output = search_palindromes('dog goat dad duck doodle never')
+# print(output) # it prints: ['dad', 'dood', 'eve']
+
+# lilli?
+
+def cut_palindrones_input_string(string):
+    words = string.split(' ')
+    return words
+
+def is_palindromes_lenght_valid(word):
+    if len(word) >= 3:
+        return True
+
+def is_palindromes(word):
+    try:
+        if is_palindromes_lenght_valid(word):
+            i = 0
+            for i in range(0, len(word)-1):
+                if word[i] == word[i+2]:
+                    return True
+                elif word[i] == word[i+1] and word[i-1] == word[i+2]:
+                    return True
+            return word
+    except IndexError:
+        pass
+
+def search_palindromes(string):
+    wordslist = cut_palindrones_input_string(string)
+    i = 0
+    while i < len(wordslist):
+        if is_palindromes(wordslist[i]):
+            print(wordslist[i])
+        i += 1
+
+search_palindromes(input('Adj meg valamit: '))
