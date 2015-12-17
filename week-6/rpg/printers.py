cadbb116b1bd2b1e0fc17f3aacc9ed9279c7bb5c
Akos Maurer-Somogyi <mrakosms@gmail.com>
2015-12-17 02:15:58 +0100

    delete files and create test folder

--- week-6/rpg/printers.py
+++
@@ -1,8 +0,0 @@
-import player
-import menu
-
-def welcome():
-    pass
-
-def hello():
-    print('Hello ', player.user.name, '! Az alabbi lehetosegek kozul valaszthatsz.', sep ='')
---
+++ week-6/rpg/tests/character_test.py
@@ -0,0 +1,15 @@
+import unittest
+from player import Player
+
+class CharacterTest(unittest.TestCase):
+    # def test_instantiate(self):
+    #     self.assertIsInstance(Character(), Character)
+
+    # def test_get_username(self):
+    #     user.name = testname
+    #     self.assertEqual(get_username(), 'testname')
+
+    pass
+
+
+unittest.main()
